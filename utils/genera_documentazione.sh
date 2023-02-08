d4764ca26aff59cd923ec445f5f4f1874c508fda
Gionata Massi <gionata.massi@savoiabenincasa.it>
2023-02-08 16:30:18 +0100

    Aggiunto diaframma ER

--- utils/genera_documentazione.sh
+++ utils/genera_documentazione.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-SCRIPT=../daSqliteAdocumentazione.js
+SCRIPT=../utils/daSqliteAdocumentazione.js
 OUTPUT=$1
 DB=${OUTPUT}.sqlite
 declare -a FORMATO=("adoc" "mermaid" "md")
@@ -7,6 +7,5 @@ declare -a FORMATO=("adoc" "mermaid" "md")
 for FMT in "${FORMATO[@]}"; do
   node ${SCRIPT} --db ${DB} --formato ${FMT} -o doc/${OUTPUT}.${FMT}
 done
-
-
+node ${SCRIPT} --db ${DB} --formato mermaid --diagramma ER -o doc/${OUTPUT}-er.${FMT}
 
