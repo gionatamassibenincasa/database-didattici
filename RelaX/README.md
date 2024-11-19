17a186b3b232e8f0b4d91b178c413df914267fca
Gionata Massi <gionata@users.noreply.github.com>
2024-11-19 08:16:51 +0100

    Add RelaX db

--- RelaX/README.md
+++ RelaX/README.md
@@ -9,3 +9,25 @@
 
 + Gist: https://gist.github.com/gionatamassibenincasa/5ed195f37313a3c3307ccbca960754c5
 + RelaX url: https://dbis-uibk.github.io/relax/calc/gist/5ed195f37313a3c3307ccbca960754c5 
+
+## Northwind
+
+Rimuovere lettere accentate!
+
++ Gist: https://gist.github.com/gionatamassibenincasa/c658c0b8d64c0e64c9af668130ec7c39
++ RelaX url: https://dbis-uibk.github.io/relax/calc/gist/c658c0b8d64c0e64c9af668130ec7c39
+
+## NW_w3schools-it
+
++ Gist: https://gist.github.com/gionatamassibenincasa/8308fc86b6925f8a907331d87ab839a7
++ RelaX url: https://dbis-uibk.github.io/relax/calc/gist/8308fc86b6925f8a907331d87ab839a7
+
+## Sakila-it
+
++ Gist: https://gist.github.com/gionatamassibenincasa/ec8484d4d72ab03786bcd7768e9eaec8
++ Relax: https://dbis-uibk.github.io/relax/calc/gist/ec8484d4d72ab03786bcd7768e9eaec8
+
+## Sakila-semplificato-it
+
++ Gist: https://gist.github.com/gionatamassibenincasa/ffeaa1bca8f4fd82c50b377d19f70e7d
++ Relax: https://dbis-uibk.github.io/relax/calc/gist/ffeaa1bca8f4fd82c50b377d19f70e7d
---
+++ utils/esporta_relax.ts
@@ -0,0 +1,50 @@
+import { Database } from "bun:sqlite";
+import { readdir } from "node:fs/promises";
+
+
+const escape = text => {
+	text = text.replaceAll("'", "`");
+	return `'${text}'`;
+};
+
+const adapt = (field) => {
+	if (typeof (field) == 'string')
+		return escape(field)
+	if (!field)
+		return 'NULL';
+	return field;
+};
+
+const main = async function () {
+	const dbPath = Bun.argv[2];
+	const dbName: string = Bun.argv[3];
+	const dbFile: string = dbPath+"/"+ dbName + ".sqlite";
+
+	const db = new Database(dbFile);
+	const tables_query = db.query("SELECT tbl_name FROM sqlite_master WHERE type='table' AND tbl_name <> 'sqlite_sequence'");
+	const tables = tables_query.all();
+
+	console.log(`group:${dbName}`);
+	console.log(`description: ${dbName} database relations (tables)`);
+	console.log('');
+
+	tables.forEach(table_name => {
+		const tbl: string = table_name.tbl_name;
+		const schema_sql: string = `SELECT name FROM pragma_table_info('${tbl}')`;
+		const schema_query = db.query(schema_sql);
+		const schema = schema_query.all();
+		console.log(`${tbl} = {${schema.map(obj => obj.name).join(",")}`);
+		const data_sql = `SELECT * FROM ${tbl}`;
+		const data_query = db.query(data_sql);
+		const data = data_query.all();
+		data.forEach(row => console.log(
+			"\t" + Object.values(row).map(adapt).join(',')
+		));
+		console.log("}\n");
+
+	});
+
+	db.close();
+}
+
+main();
